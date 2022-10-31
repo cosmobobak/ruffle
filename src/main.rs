a3bbac5886021f7a064bac190eaf2c101c253b4d
cosmobobak <cosmobobak@gmail.com>
2022-10-31 16:21:40 +0000

    v2

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,32 @@
-use std::io::{self, Write};
+use std::io::{self, Write, Read};
 
 use rand::seq::SliceRandom;
 
 fn main() -> io::Result<()> {
     let args = std::env::args().collect::<Vec<_>>();
-    if args.len() != 2 {
-        println!("Usage: {} <file>", args[0]);
-        return Ok(());
+    let ref_args = args.iter().map(|s| s.as_str()).collect::<Vec<_>>();
+    match ref_args[..] {
+        [_] | [_, "-"] => {
+            let mut buf = Vec::new();
+            // read from stdin
+            let _bytes_read = io::stdin().lock().read_to_end(&mut buf)?;
+            driver(&buf)
+        }
+        [_, filename] => {
+            let file = std::fs::File::open(filename)?;
+            let mmap = unsafe { memmap::Mmap::map(&file)? };
+            driver(&mmap)
+        }
+        _ => {
+            eprintln!("ruffle: extra operand '{}'", ref_args[2]);
+            std::process::exit(1);
+        }
     }
+}
+
+fn driver(bytes: &[u8]) -> Result<(), io::Error> {
     let output = std::io::stdout();
-    let file = std::fs::File::open(&args[1])?;
-    let mmap = unsafe { memmap::Mmap::map(&file)? };
-    let mut lines = mmap.split(|&c| c == b'\n').collect::<Vec<_>>();
+    let mut lines = bytes.split(|&c| c == b'\n').collect::<Vec<_>>();
     lines.shuffle(&mut rand::thread_rng());
     let mut output = std::io::BufWriter::new(output.lock());
     for line in lines {
