508f19e347c7e26db9d1edc57cb28d3b33f3f15a
cosmobobak <cosmobobak@gmail.com>
2022-10-31 16:02:54 +0000

    v1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
+use std::io::{self, Write};
 
+use rand::seq::SliceRandom;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> io::Result<()> {
+    let args = std::env::args().collect::<Vec<_>>();
+    if args.len() != 2 {
+        println!("Usage: {} <file>", args[0]);
+        return Ok(());
+    }
+    let output = std::io::stdout();
+    let file = std::fs::File::open(&args[1])?;
+    let mmap = unsafe { memmap::Mmap::map(&file)? };
+    let mut lines = mmap.split(|&c| c == b'\n').collect::<Vec<_>>();
+    lines.shuffle(&mut rand::thread_rng());
+    let mut output = std::io::BufWriter::new(output.lock());
+    for line in lines {
+        output.write_all(line)?;
+        output.write_all(b"\n")?;
+    }
+    Ok(())
 }
